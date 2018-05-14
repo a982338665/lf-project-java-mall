ea9c7323816ec7a1ce33718bcbfaa62e2ff7d3f4
a982338665 <982338665@qq.com>
2018-05-14 18:04:09 +0800

    ftp服务器

--- bobo-mall/bobo-manager/bobo-manager-web/src/main/java/com/bobo/controller/PageController.java
+++ bobo-mall/bobo-manager/bobo-manager-web/src/main/java/com/bobo/controller/PageController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * 页面跳转controller
  * @version 1.0
@@ -11,11 +15,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
+
 	/**
 	 * 打开首页
 	 */
 	@RequestMapping("/")
-	public String showIndex() {
+	public String showIndex(final HttpServletResponse response) {
+		/*new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					response.sendRedirect("http://www.baidu.com");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();*/
 		return "index";
 	}
 	/**
